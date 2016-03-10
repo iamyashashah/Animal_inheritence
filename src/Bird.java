c9894328c0b454b37e51160f4a8751927bf23a32
iamyashashah <yashshah.1512@gmail.com>
2016-03-10 11:37:23 -0500

    first commit

---
+++ src/Bird.java
@@ -0,0 +1,25 @@
+
+public class Bird extends Animal{
+	
+	public Bird()
+	{
+		super();
+		System.out.println("A new bird has been created");
+	}
+	
+	public void sleep()
+	{
+		System.out.println("A bird sleeps....");
+	}
+	
+	public void eat()
+	{
+		System.out.println("A bird eats.....");
+	}
+	
+	public void fly()
+	{
+		System.out.println("A bird can fly...");
+	}
+
+}
---
+++ src/Cat.java
@@ -0,0 +1,18 @@
+
+public class Cat extends Animal {
+	public Cat()
+	{
+		super();
+		System.out.println("A new cat has been created");
+	}
+	
+	public void sleep()
+	{
+		System.out.println("A cat sleeps....");
+	}
+	
+	public void eat()
+	{
+		System.out.println("A cat eats.....");
+	}
+}
